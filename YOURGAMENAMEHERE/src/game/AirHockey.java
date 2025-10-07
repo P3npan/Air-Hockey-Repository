4dfdd085299884b285134eefe00a8d9426da98eb
PranavThe <pranavpuranik07@gmail.com>
2025-10-07 15:43:37 -0400

    Game board setup

--- YOURGAMENAMEHERE/src/game/AirHockey.java
+++ YOURGAMENAMEHERE/src/game/AirHockey.java
@@ -10,19 +10,34 @@ NOTE: This class is the metaphorical "main method" of your program,
 import java.awt.*;
 import java.awt.event.*;
 
-class YourGameName extends Game {
+class AirHockey extends Game {
 	static int counter = 0;
 
-  public YourGameName() {
-    super("YourGameName!",800,600);
+  public AirHockey() {
+    super("Air Hockey",800,400);
     this.setFocusable(true);
 	this.requestFocus();
   }
   
 	public void paint(Graphics brush) {
-    	brush.setColor(Color.black);
+    	brush.setColor(Color.white);
     	brush.fillRect(0,0,width,height);
-    	
+    	brush.setColor(Color.red);
+    	brush.drawOval(-75, 125, 150, 150);
+    	brush.drawOval(725, 125, 150, 150);
+    	brush.drawOval(360, 160, 80, 80);
+    	brush.fillOval(390, 190, 20, 20);
+    	brush.fillRect(399, 0, 2, 400);
+    	brush.setColor(Color.red);
+    	brush.fillRect(0, 125, 8, 150);
+    	brush.fillRect(792, 125, 8, 150);
+    	brush.setColor(Color.black);
+    	brush.fillRect(0, 0, 8, 127);
+    	brush.fillRect(0, 273, 8, 150);
+    	brush.fillRect(792, 0, 8, 127);
+    	brush.fillRect(792, 273, 8, 150);
+    	brush.fillRect(0, 0, 800, 8);
+    	brush.fillRect(0, 364, 800, 8);
     	// sample code for printing message for debugging
     	// counter is incremented and this message printed
     	// each time the canvas is repainted
@@ -32,7 +47,7 @@ class YourGameName extends Game {
   }
   
 	public static void main (String[] args) {
-   		YourGameName a = new YourGameName();
+   		AirHockey a = new AirHockey();
 		a.repaint();
   }
 }
