2dae49c2f267639ab6174d0a0a10ac99d8fa676f
PranavThe <pranavpuranik07@gmail.com>
2025-10-15 09:35:27 -0400

    Board and Border Classes

--- YOURGAMENAMEHERE/src/game/AirHockey.java
+++ YOURGAMENAMEHERE/src/game/AirHockey.java
@@ -29,29 +29,9 @@ class AirHockey extends Game implements KeyListener {
         brush.fillOval(390, 190, 20, 20);
         brush.fillRect(399, 0, 2, 400);
 
-        Polygon goalLeft = makeRectangle(0, 125, 8, 150);
-        drawRectangle(goalLeft, brush, Color.red);
+        Board airHockey = new Board();
+        airHockey.drawAllBorders(brush);
 
-        Polygon goalRight = makeRectangle(792, 125, 8, 150);
-        drawRectangle(goalRight, brush, Color.red);
-
-        Polygon leftTopBorder = makeRectangle(0, 0, 8, 127);
-        drawRectangle(leftTopBorder, brush, Color.black);
-
-        Polygon leftBottomBorder = makeRectangle(0, 273, 8, 150);
-        drawRectangle(leftBottomBorder, brush, Color.black);
-
-        Polygon rightTopBorder = makeRectangle(792, 0, 8, 127);
-        drawRectangle(rightTopBorder, brush, Color.black);
-
-        Polygon rightBottomBorder = makeRectangle(792, 273, 8, 150);
-        drawRectangle(rightBottomBorder, brush, Color.black);
-
-        Polygon topBorder = makeRectangle(0, 0, 800, 8);
-        drawRectangle(topBorder, brush, Color.black);
-
-        Polygon bottomBorder = makeRectangle(0, 392, 800, 8);
-        drawRectangle(bottomBorder, brush, Color.black);
 
         updatePucks();
 
@@ -64,29 +44,6 @@ class AirHockey extends Game implements KeyListener {
         counter++;
         brush.setColor(Color.white);
         brush.drawString("Counter is " + counter, 10, 10);
-    }
-
-    public Polygon makeRectangle(int x_position, int y_position, int width, int height) {
-        Point[] rectPoints = {
-            new Point(0, 0),
-            new Point(width, 0),
-            new Point(width, height),
-            new Point(0, height)
-        };
-        Polygon rectangle = new Polygon(rectPoints, new Point(x_position, y_position), 0);
-        return rectangle;
-    }
-
-    public void drawRectangle(Polygon rectangle, Graphics brush, Color color) {
-        Point[] points = rectangle.getPoints();
-        int[] x_points = new int[points.length];
-        int[] y_points = new int[points.length];
-        for (int i = 0; i < points.length; i++) {
-            x_points[i] = (int) points[i].x;
-            y_points[i] = (int) points[i].y;
-        }
-        brush.setColor(color);
-        brush.fillPolygon(x_points, y_points, points.length);
     }
 
     public void updatePucks() {
